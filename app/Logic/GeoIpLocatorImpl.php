1d27dab56963be7af66e39dbf9acc60131e39ffc
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2022-10-12 10:16:54 +0200

    Added disaster recovery in MaxMind DB

--- app/Logic/GeoIpLocatorImpl.php
+++ app/Logic/GeoIpLocatorImpl.php
@@ -3,8 +3,13 @@
 namespace App\Logic;
 
 use GeoIp2\Database\Reader;
+use GeoIp2\Exception\AddressNotFoundException;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use MaxMind\Db\Reader\InvalidDatabaseException;
 
-class GeoIpLocatorImpl implements GeoIpLocator {
+class GeoIpLocatorImpl implements GeoIpLocator
+{
 
     private readonly Reader $resolver;
 
@@ -18,7 +23,30 @@ class GeoIpLocatorImpl implements GeoIpLocator {
      */
     public function locate(String $ip): String
     {
-        return $this->resolver->country($ip)->country->isoCode;
+        return $this->retryLocate($ip);
     }
 
+    /**
+     * Allows to try X times the recovery from a invalid database.
+     *
+     * @param String ip to look for in the database
+     * @param int current retry of the recursion, max 3 tries
+     *
+     * @return String location of IP, '--' if not in database
+     */
+    public function retryLocate(String $ip, int $retries = 0): String
+    {
+        try {
+            return $this->resolver->country($ip)->country->isoCode;
+        } catch (AddressNotFoundException) {
+            return '--';
+        } catch (InvalidDatabaseException $exception) {
+            $returnCode = Artisan::call('app:geoip:download');
+            if ($returnCode != 0 && $retries >= 3) {
+                Log::emergency("Cannot recover from invalid MaxMind DB.");
+                throw $exception;
+            }
+            return $this->retryLocate($ip, $retries + 1);
+        }
+    }
 }
