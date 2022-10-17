6c33339c69d1727ffe42ba43aaed15445fa47eff
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2022-10-17 11:28:48 +0200

    Added installations reverse relationships

--- app/Models/Version.php
+++ app/Models/Version.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Version extends Model
 {
     use HasFactory;
+
+    /**
+     * Get all of the installations for the Version
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function installations(): HasMany
+    {
+        return $this->hasMany(Installation::class);
+    }
 }
