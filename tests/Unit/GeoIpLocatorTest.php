776f93fc7d9b2946fe3185afcae7725e67a0659b
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2022-10-20 12:38:30 +0200

    Added tests for GeoIpLocatorImpl

---
+++ tests/Unit/GeoIpLocatorTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Logic\GeoIpLocatorImpl;
+use GeoIp2\Database\Reader;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use MaxMind\Db\Reader\InvalidDatabaseException;
+use Mockery\MockInterface;
+
+test('invalid database present', function() {
+    $reader = Mockery::mock(Reader::class, function (MockInterface $mock) {
+        $mock->shouldReceive('country')
+            ->with('127.0.0.1')
+            ->andThrow(new InvalidDatabaseException());
+    });
+
+    Artisan::partialMock()
+        ->shouldReceive('call')
+        ->times(3)
+        ->with('app:geoip:download');
+
+    Log::partialMock()
+        ->shouldReceive('emergency')
+        ->once()
+        ->with('Cannot recover from invalid MaxMind DB.');
+
+    $geoIpLocator = new GeoIpLocatorImpl($reader);
+    $geoIpLocator->locate('127.0.0.1');
+})->throws(InvalidDatabaseException::class);
