de5f8d6d1a70e25f1ba0e7a7b399480d47c6c3ff
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2022-10-26 14:25:26 +0200

    Updated static wait-for with dynamic one

--- containers/nginx/start.sh
+++ containers/nginx/start.sh
@@ -4,6 +4,6 @@ set -e
 # This ensures the command "nginx" waits, while everything else runs without delays.
 if [ "$1" = "nginx" ]; then
     echo "Wait for PHP backend to come up..."
-    wait-for -t 30 "app:9000"
+    wait-for -t 30 "${FPM_URL}:${FPM_PORT}"
 fi
 exec /docker-entrypoint.sh "$@"
