fbd3ea6f100e889ed55313a29ab75228f18b6822
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2022-11-02 12:21:06 +0100

    Using buildah/podman to build the containers

--- deploy/ns8/build-images.sh
+++ deploy/ns8/build-images.sh
@@ -1,67 +1,43 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 #
 # Copyright (C) 2022 Nethesis S.r.l.
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-# Terminate on error
 set -e
 
-# Prepare variables for later use
-images=()
-# The image will be pushed to GitHub container registry
-repobase="${REPOBASE:-ghcr.io/nethserver}"
-# Configure the image name
-reponame="phonehome"
+# Set up all variables needed to build the arguments
+tags=${TAGS:-ghcr.io/tbaile/phonehome:latest}
+labels=${LABELS:-}
+args=${ARGS:-}
 
-# Create a new empty container image
-container=$(buildah from scratch)
+for tag in $tags; do
+    set -- "$@" --tag="$tag"
+done
 
-# Reuse existing nodebuilder-phonehome container, to speed up builds
-if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-phonehome; then
-    echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-phonehome -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
-fi
+for label in $labels; do
+    set -- "$@" --label="$label"
+done
 
-echo "Build static UI files with node..."
-buildah run nodebuilder-phonehome sh -c "cd /usr/src/ui && yarn install && yarn build"
+for arg in $args; do
+    set -- "$@" --build-arg="$arg"
+done
 
-# Add imageroot directory to the container image
-buildah add "${container}" imageroot /imageroot
-buildah add "${container}" ui/dist /ui
-# Setup the entrypoint, ask to reserve one TCP port with the label and set a rootless container
-buildah config --entrypoint=/ \
-    --label="org.nethserver.authorizations=traefik@any:routeadm" \
-    --label="org.nethserver.tcp-ports-demand=1" \
-    --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mariadb:10.6.10" \
-    "${container}"
-# Commit the image
-buildah commit "${container}" "${repobase}/${reponame}"
+# Execute the buildah build command with the additional arguments given above
+buildah build \
+    --force-rm \
+    --jobs "$(nproc)" \
+    --layers \
+    --file Dockerfile \
+    --target production \
+    "$@"
+    .
 
-# Append the image URL to the images array
-images+=("${repobase}/${reponame}")
-
-#
-# NOTICE:
-#
-# It is possible to build and publish multiple images.
-#
-# 1. create another buildah container
-# 2. add things to it and commit it
-# 3. append the image url to the images array
-#
-
-#
-# Setup CI when pushing to Github.
-# Warning! docker::// protocol expects lowercase letters (,,)
-if [[ -n "${CI}" ]]; then
-    # Set output value for Github Actions
-    printf "::set-output name=images::%s\n" "${images[*],,}"
-else
-    # Just print info for manual push
-    printf "Publish the images with:\n\n"
-    for image in "${images[@],,}"; do printf "  buildah push %s docker://%s:%s\n" "${image}" "${image}" "${IMAGETAG:-latest}" ; done
-    printf "\n"
+# If it's run locally, print the next commands to publish the images tagged
+if [ -z "${CI}" ]; then
+    echo "Manually push the images with:"
+    for tag in $tags; do
+        echo "podman push $tag"
+    done
 fi
