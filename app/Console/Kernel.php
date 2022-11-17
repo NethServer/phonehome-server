759bb4c21651c522ff4b767b71540c285b377c7f
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2022-11-17 12:01:22 +0100

    Scheduling GeoIP database fetch daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(GeoIPDownloadCommand::class)->monthly();
+        $schedule->command(GeoIPDownloadCommand::class)->daily();
     }
 
     /**
