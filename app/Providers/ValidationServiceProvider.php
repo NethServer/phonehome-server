8389038e6741e07d9ab18d994a650605af42646d
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2022-12-07 16:47:42 +0100

    Added Schema definitions
    Added schemas to Opis\JsonSchema\Validator with service provider

---
+++ app/Providers/ValidationServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+use Opis\JsonSchema\Resolvers\SchemaResolver;
+use Opis\JsonSchema\Validator;
+
+class ValidationServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(Validator::class, function () {
+            // Register all schemas under resources/validation
+            $resolver = new SchemaResolver();
+            $resolver->registerPrefix(
+                'https://schema.nethserver.org/',
+                resource_path('validation')
+            );
+            $validator = new Validator();
+            $validator->setResolver($resolver);
+
+            return $validator;
+        });
+    }
+
+    public function provides(): array
+    {
+        return [Validator::class];
+    }
+}
--- config/app.php
+++ config/app.php
@@ -195,6 +195,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\GeoIpProvider::class,
+        App\Providers\ValidationServiceProvider::class,
 
     ],
 
