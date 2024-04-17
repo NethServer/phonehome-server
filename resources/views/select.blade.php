05173f41f767d62094b8bc503d6ba109984564e2
PietroComelli <comelli.pietro@gmail.com>
2024-04-17 17:35:43 +0200

    feat: ui improvements
    - Edit CSS
    - Updated views
    - Added Map link

--- resources/views/select.blade.php
+++ resources/views/select.blade.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Select Hardware</title>
-    <link rel="stylesheet" href= "css/select.css">
+    <link rel="stylesheet" href= "{{ asset('css/select.css') }}">
+    <link rel="icon" href="{{ asset('images/logoNethesis.png') }}">
 </head>
 <body>
     <div class="container">
         <h1>Select Hardware</h1>    
-        <button> <a href= "{{ route('hardware', ['installation' => 'NethServer'] )}}"><strong>NethServer</strong></a></button>
-        <button> <a href= "{{ route('hardware', ['installation' => 'NethSecurity'] )}}"><strong>NethSecurity</strong></a></button>
+        <a href= "{{ route('hardware', ['installation' => 'NethServer'] )}}"><button><strong>NethServer</strong></button></a>
+        <a href= "{{ route('hardware', ['installation' => 'NethSecurity'] )}}"><button><strong>NethSecurity</strong></button></a>
     </div>
 </body>
 </html>
